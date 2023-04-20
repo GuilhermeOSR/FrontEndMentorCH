697ba773d9581b4b41846c3056da51d1a89ffb4f
GuilhermeOSR <guilherme.o.santarosa>
2023-04-20 14:21:27 -0300

    teste

--- FrontEndMentor-Stats-Preview-Card/script.js
+++ FrontEndMentor-Stats-Preview-Card/script.js
@@ -1,7 +1,12 @@
-const container = document.querySelector("img");
+const container = document.querySelectors("img");
+
 
 function checkWith() {
-    if(window.innerWidth < 859) {
+   
+    if(window.innerWidth > 859) {
+        container.style.display='none';
+    }
+    else if(window.innerWidth < 859) {
         container.style.display='block';
     }
     else {
